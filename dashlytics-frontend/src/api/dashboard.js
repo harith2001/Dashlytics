784e61e37735a2b7e034c94b7d9e6affd2e3b14f
harith vithanage <danula243@gmail.com>
2025-09-14 23:27:44 +0530

    API added

--- dashlytics-frontend/src/api/dashboard.js
+++ dashlytics-frontend/src/api/dashboard.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
 
 //limit can be changed to get more data
 
 export const getCountryRevenue = (limit) => 
-    axios.get(`${API_BASE_URL}/country-revenue?limit=${limit || 10}`)  
+    axios.get(`${API_BASE_URL}/api/v1/country-revenue?limit=${limit || 10}`)  
 
 export const getTopProducts = () =>
-    axios.get(`${API_BASE_URL}/top-products`)  
+    axios.get(`${API_BASE_URL}/api/v1/top-products`)  
 
 export const getMonthlySales = (sortField, sortOrder) =>
-    axios.get(`${API_BASE_URL}/monthly-sales?sortField=${sortField || 'month'}&sortOrder=${sortOrder || 'asc'}`)
+    axios.get(`${API_BASE_URL}/api/v1/monthly-sales?sortField=${sortField || 'month'}&sortOrder=${sortOrder || 'asc'}`)
 
 export const getTopRegions = (limit) =>
-    axios.get(`${API_BASE_URL}/top-regions?limit=${limit || 30}`)
+    axios.get(`${API_BASE_URL}/api/v1/top-regions?limit=${limit || 30}`)
 
